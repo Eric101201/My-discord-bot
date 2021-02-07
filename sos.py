61c3274b595517c46e61048898bf4721fcc11909
Eric101201 <z077452958>
2021-02-07 14:43:38 +0800

    add areaName/areaDesc(?

--- sos.py
+++ sos.py
@@ -1,14 +1,15 @@
-import json
 import re
 import discord
 import feedparser
 import requests
 import datetime
+import asyncio
 
 async def bigsos(channel, API):
   r = requests.get(API)
   eew = r.json()
   inp = eew["records"]["earthquake"][0]
+  aaa = ['最大震度1級地區', '最大震度2級地區', '最大震度3級地區', '最大震度4級地區', '最大震度5級地區', '最大震度6級地區', '最大震度7級地區', '最大震度8級地區']
   inpinfo = inp["earthquakeInfo"]
   helpawa = inp["web"]                                    #資料連結
   earthquakeNo = inp["earthquakeNo"]                      #幾號地震
@@ -18,6 +19,7 @@ async def bigsos(channel, API):
   magnitudeValue = inpinfo["magnitude"]["magnitudeValue"] #規模單位
   value = inpinfo["depth"]["value"]                       #地震深度
   unit = inpinfo["depth"]["unit"]                         #深度單位
+  await asyncio.sleep(5)
   urlicon = inp["reportImageURI"]                         #深度單位
   embed = discord.Embed(title=eew['records']['datasetDescription'], color=0xff0000)
   embed.set_author(name="台灣地震報告系統", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527807435112478/EEW.png')
@@ -28,6 +30,13 @@ async def bigsos(channel, API):
   embed.add_field(name="發生時間", value=f"{originTime}", inline=True)            #發生時間
   embed.add_field(name=f"{magnitdueType}", value=f"{magnitudeValue}", inline=True) #規模
   embed.add_field(name="深度", value=f"{value}{unit}", inline=True)               #發生時間
+  for i in aaa:
+    aaaa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    for a in range(len(aaaa)):
+      owowowo = inp["intensity"]["shakingArea"][aaaa[a]]["areaDesc"]
+      if owowowo in i:
+        owowowoa = inp["intensity"]["shakingArea"][aaaa[a]]["areaName"]
+        embed.add_field(name=i, value=owowowoa, inline=False)
   embed.set_footer(text="地震報告提供", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527875839885312/ROC_CWB.png')
   await channel.send(embed=embed)
 
@@ -35,6 +44,7 @@ async def smsos(channel, API2):
   r = requests.get(API2)
   eew = r.json()
   inp = eew["records"]["earthquake"][0]
+  aaa = ['最大震度1級地區', '最大震度2級地區', '最大震度3級地區', '最大震度4級地區', '最大震度5級地區', '最大震度6級地區', '最大震度7級地區', '最大震度8級地區']
   inpinfo = inp["earthquakeInfo"]
   helpawa = inp["web"]                                    #資料連結
   earthquakeNo = inp["earthquakeNo"]                      #幾號地震
@@ -44,6 +54,7 @@ async def smsos(channel, API2):
   magnitudeValue = inpinfo["magnitude"]["magnitudeValue"] #規模單位
   value = inpinfo["depth"]["value"]                       #地震深度
   unit = inpinfo["depth"]["unit"]                         #深度單位
+  await asyncio.sleep(5)
   urlicon = inp["reportImageURI"]                         #深度單位
   embed = discord.Embed(title=eew['records']['datasetDescription'], color=0xff0000)
   embed.set_author(name="台灣地震報告系統", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527807435112478/EEW.png')
@@ -54,6 +65,13 @@ async def smsos(channel, API2):
   embed.add_field(name="發生時間", value=f"{originTime}", inline=True)            #發生時間
   embed.add_field(name=f"{magnitdueType}", value=f"{magnitudeValue}", inline=True) #規模
   embed.add_field(name="深度", value=f"{value}{unit}", inline=True)               #發生時間
+  for i in aaa:
+    aaaa = [0, 1, 2, 3]
+    for a in range(len(aaaa)):
+      owowowo = inp["intensity"]["shakingArea"][aaaa[a]]["areaDesc"]
+      if owowowo in i:
+        owowowoa = inp["intensity"]["shakingArea"][aaaa[a]]["areaName"]
+        embed.add_field(name=i, value=owowowoa, inline=False)
   embed.set_footer(text="地震報告提供", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527875839885312/ROC_CWB.png')
   await channel.send(embed=embed)
 
@@ -62,12 +80,14 @@ async def NEWMOHW(channel, url):
   oaoa = rss['entries'][0]['title']
   owow = rss.entries[0]['summary']
   link = rss.entries[0]['link']
+  covtime = rss["entries"][0]["published"]
   text = re.sub("<.*?>", "", owow)
   nowtime = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
   embed = discord.Embed(title=f'{oaoa}', color=discord.Colour.blue())
   embed.set_author(name='衛生福利部公告',
                     icon_url='https://images-ext-1.discordapp.net/external/xrfvu0X7I_vcTEmPlp0x5JqmlM9D17azlTEbYTOVFlM/https/upload.wikimedia.org/wikipedia/commons/thumb/a/a3/ROC_Ministry_of_Health_and_Welfare_Seal.svg/1200px-ROC_Ministry_of_Health_and_Welfare_Seal.svg.png?width=677&height=677')
-  embed.add_field(name='新聞連結', value=f'[點擊此處]({link})', inline=False)
+  embed.add_field(name='新聞連結', value=f'[點擊此處]({link})', inline=True)
+  embed.add_field(name='發布時間', value=f'{covtime}', inline=True)
   embed.add_field(name='內容', value=f'{text}', inline=False)
   embed.set_footer(text=f'衛生福利部RSS服務提供• {nowtime} ',
                     icon_url='https://images-ext-1.discordapp.net/external/xrfvu0X7I_vcTEmPlp0x5JqmlM9D17azlTEbYTOVFlM/https/upload.wikimedia.org/wikipedia/commons/thumb/a/a3/ROC_Ministry_of_Health_and_Welfare_Seal.svg/1200px-ROC_Ministry_of_Health_and_Welfare_Seal.svg.png?width=677&height=677')
