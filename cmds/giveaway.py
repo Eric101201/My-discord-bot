29faa7e3f9a84f298bbcb9d230129e385c805fdf
Eric101201 <z077452958>
2021-02-08 23:26:24 +0800

    0.4

--- cmds/giveaway.py
+++ cmds/giveaway.py
@@ -1,9 +1,8 @@
 import discord
 import asyncio
 import random
-import time
 
-from datetime import datetime
+import datetime
 from discord.ext import commands
 
 prefix = 'w+'
@@ -30,66 +29,14 @@ class giveaway(commands.Cog):
         self.bot = bot
 
     @commands.has_guild_permissions(administrator=True)
-    @commands.command(name='gstart', help='抽獎系統 <抽獎倒數時間> <抽獎內容>')
+    @commands.command(name='gstart', help='抽獎系統 <抽獎倒數時間> <數量> <抽獎內容>')
     async def gstart(self, ctx, mins, num:int, *, prize:str):
-        _weekday = {
-            0: '星期一',
-            1: '星期二',
-            2: '星期三',
-            3: '星期四',
-            4: '星期五',
-            5: '星期六',
-            6: '星期日'
-        }
-
-        # 格林威治天文臺時間
-        t = time.gmtime(time.time())
-
-        # 月份天數陣列
-        day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-        # 轉成代數
-        y = t.tm_year
-        M = t.tm_mon
-        d = t.tm_mday
-        h = t.tm_hour + 8
-        m = t.tm_min
-        s = t.tm_sec
-
-        # 判斷潤平年
-        if y % 400 == 0:
-            day[2] = 29
-        elif y % 100 == 0:
-            day[2] = 28
-        elif y % 4 == 0:
-            day[2] = 29
-        else:
-            day[2] = 28
-
-        # 處裡跨月跨年狀況
-        d = d + int(h / 24)
-        h = h % 24
-        tm = day[M]
-        M = M + int(d / tm)
-        d = d % tm
-        y = y + int(M / 12)
-        M = M % 12
-
-        # s 秒
-        # m 分
-        # h 時
-        # d 日
-        # M 月
-        # y 年
-
-        g = (_weekday[datetime.today().weekday()])
-        txt = "{} {}:{}"
-        txt2 = "{}/{}/{} {}"
+        nowtime = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
         time2 = convert(mins)
 
         embed = discord.Embed(title='抽獎系統!', color=discord.Colour.orange())
         embed.add_field(name='抽獎獎品:', value=f'`{prize}`', inline=False)
-        embed.add_field(name='開始時間:', value=f'{datetime.now()}''', inline=False)
+        embed.add_field(name='開始時間:', value=f'{nowtime}', inline=False)
         embed.set_footer(text=f'結束於 {mins} 後')
 
         my_msg = await ctx.send(embed=embed)
