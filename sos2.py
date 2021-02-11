a28b854a529dfbe2b94a8c03529d7ed044fb4aa8
Eric101201 <z077452958>
2021-02-12 01:50:29 +0800

    不一定採用先保存

--- sos2.py
+++ sos2.py
@@ -4,6 +4,7 @@ import feedparser
 import requests
 import datetime
 import asyncio
+import urllib.request
 
 async def bigsos(channel, API):
   await asyncio.sleep(5)
@@ -39,7 +40,8 @@ async def bigsos(channel, API):
     embed.add_field(name=i['desc'], value=i['name'], inline=False)
   embed.set_footer(text="地震報告提供", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527875839885312/ROC_CWB.png')
   await channel.send(embed=embed)
-  await channel.send(urlicon)
+  urllib.request.urlretrieve(urlicon, 'bigsos.png')
+  await channel.send(file=discord.File("bigsos.png"))
 
 async def smsos(channel, API2):
   await asyncio.sleep(5)
@@ -49,23 +51,23 @@ async def smsos(channel, API2):
   owwww = inp["intensity"]["shakingArea"]
   shaking = []
   inpinfo = inp["earthquakeInfo"]
-  helpawa = inp["web"]
-  earthquakeNo = inp["earthquakeNo"] 
-  location = inpinfo["epiCenter"]["location"]
-  originTime = inpinfo["originTime"] 
-  magnitdueType = inpinfo["magnitude"]["magnitdueType"] 
-  magnitudeValue = inpinfo["magnitude"]["magnitudeValue"]
-  value = inpinfo["depth"]["value"]
-  unit = inpinfo["depth"]["unit"]
-  urlicon = inp["reportImageURI"]
+  helpawa = inp["web"]                                    #資料連結
+  earthquakeNo = inp["earthquakeNo"]                      #幾號地震
+  location = inpinfo["epiCenter"]["location"]             #發生地點
+  originTime = inpinfo["originTime"]                      #發生時間
+  magnitdueType = inpinfo["magnitude"]["magnitdueType"]   #規模單位
+  magnitudeValue = inpinfo["magnitude"]["magnitudeValue"] #規模單位
+  value = inpinfo["depth"]["value"]                       #地震深度
+  unit = inpinfo["depth"]["unit"]                         #深度單位
+  urlicon = inp["reportImageURI"]                         #深度單位
   embed = discord.Embed(title=eew['records']['datasetDescription'], color=0xff0000)
   embed.set_author(name="台灣地震報告系統", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527807435112478/EEW.png')
-  embed.add_field(name="報告連結", value=f"[中央氣象局]({helpawa})", inline=True)
-  embed.add_field(name="編號", value=f"{earthquakeNo}", inline=True) 
-  embed.add_field(name="震央位置", value=f"{location}", inline=True) 
-  embed.add_field(name="發生時間", value=f"{originTime}", inline=True)
-  embed.add_field(name=f"{magnitdueType}", value=f"{magnitudeValue}", inline=True)
-  embed.add_field(name="深度", value=f"{value}{unit}", inline=True)
+  embed.add_field(name="報告連結", value=f"[中央氣象局]({helpawa})", inline=True)  #報告連結
+  embed.add_field(name="編號", value=f"{earthquakeNo}", inline=True)              #編號
+  embed.add_field(name="震央位置", value=f"{location}", inline=True)              #震央位置
+  embed.add_field(name="發生時間", value=f"{originTime}", inline=True)            #發生時間
+  embed.add_field(name=f"{magnitdueType}", value=f"{magnitudeValue}", inline=True) #規模
+  embed.add_field(name="深度", value=f"{value}{unit}", inline=True)               #發生時間
   embed.set_image(url=f"{urlicon}")
   for i in owwww:
     if '最' in i['areaDesc']:
@@ -75,7 +77,8 @@ async def smsos(channel, API2):
     embed.add_field(name=i['desc'], value=i['name'], inline=False)
   embed.set_footer(text="地震報告提供", icon_url='https://media.discordapp.net/attachments/345147297539162115/732527875839885312/ROC_CWB.png')
   await channel.send(embed=embed)
-  await channel.send(urlicon)
+  urllib.request.urlretrieve(urlicon, 'smsos.png')
+  await channel.send(file=discord.File("smsos.png"))
 
 async def NEWMOHW(channel, url):
   rss = feedparser.parse(url)
